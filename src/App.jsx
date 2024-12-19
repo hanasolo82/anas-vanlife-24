eb6f66a44289a3742b25b5157b10eb543219ab19
Hanasolo <hanasolo@hotmail.com>
2024-12-19 10:15:54 +0100

    deleted images and added server and vans

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import About from './Pages/About';
 import Home from './Pages/Home';
-
+import './server'
 
 
 export default function App() {
   
   return (
     <BrowserRouter>
-      <nav>
+      <nav className='navar'>
+        <h1 className='navbar-logo'>#VANLIFE</h1>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
       </nav>
@@ -16,6 +17,7 @@ export default function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>.
       </Routes>
+      <footer className='footer'>Ⓒ 2022 #VANLIFE</footer>
     </BrowserRouter>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,19 @@ export default function Home() {
 
     return (
         <div>
-            Home
+                <img className='home-img'/>
+                <title>
+                   You got the travel plans, we got the travel vans. 
+                </title>
+                <p>
+                    Add adventure to your life by joining the #vanlife movement.
+                    <br/>
+                    Rent the perfect van to make your perfect road trip.
+                </p>
+                <button>
+                    Find your van
+                </button>
+            
         </div>
     )
 }
